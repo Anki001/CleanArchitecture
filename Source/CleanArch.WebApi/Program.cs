a9c14102947cf5c3b48f15ace384ed72b40176e5
Ankush Ashok <ankush_gawande@jabil.com>
2023-03-22 12:01:01 +0800

    Ankush - Master copy of clean architecture

---
+++ Source/CleanArch.WebApi/Program.cs
@@ -0,0 +1,35 @@
+using CleanArch.Application;
+using CleanArch.Infrastructure;
+using CleanArch.Presentation;
+using Serilog;
+
+var builder = WebApplication.CreateBuilder(args);
+
+// Add services to the container.
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
+
+// Add dependency of Application, Infrastructure and Presentaion layer
+builder.Services
+    .AddApplication()
+    .AddInfrastructure()
+    .AddPresentation();
+
+// Register loger configuration
+builder.Host.UseSerilog((context, logConfiguration) =>
+    logConfiguration.ReadFrom.Configuration(context.Configuration));
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseSerilogRequestLogging();
+
+app.UseHttpsRedirection();
+
+app.Run();
